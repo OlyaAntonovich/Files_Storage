73d6693a4a38ed1acebcbd1331ea87c7c798d6ff
Olya Antonovich <ynonaj@gmail.com>
2023-05-15 23:37:54 +0300

    The second task with mistakes

--- app/src/main/java/com/example/files_storage/fragments/FragmentFirst.kt
+++ app/src/main/java/com/example/files_storage/fragments/FragmentFirst.kt
@@ -4,10 +4,12 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
@@ -20,15 +22,19 @@ import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.files_storage.MainActivity
 import com.example.files_storage.adapter.Adapter
 import com.example.files_storage.data.Item
 import com.example.files_storage.databinding.FragmentFirstBinding
 import com.example.files_storage.service.Permissions
 import com.example.files_storage.service.WriteOpenExternalMemory
+import com.example.files_storage.service.WriteOpenExternalMemory.Companion.FILE_EXTERNAL_NAME
 import com.example.files_storage.service.WriteOpenFile
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.divider.MaterialDividerItemDecoration
+import java.io.BufferedWriter
 import java.io.File
+import java.io.FileWriter
 import java.security.Permission
 
 
@@ -37,11 +43,20 @@ class FragmentFirst : Fragment() {
     private val binding get() = requireNotNull(_binding)
     private var items = mutableListOf<Item>()
     private var itemsFromFile = mutableListOf<Item>()
-//    private val permissionLauncher = registerForActivityResult(
-//        ActivityResultContracts.RequestMultiplePermissions()
-//    ) {
-//
-//    }
+
+    private val hasReadPermissions: Boolean
+        get() = requireContext().hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    private val hasWritePermissions: Boolean
+        get() = requireContext().hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) {
+
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -57,13 +72,13 @@ class FragmentFirst : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        permissionLauncher.launch(
-//            arrayOf(
-//                Manifest.permission.READ_EXTERNAL_STORAGE,
-//                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//                Manifest.permission.MANAGE_EXTERNAL_STORAGE
-//            )
-//        )
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+
+                )
+        )
 
         isFileExists()
 
@@ -102,20 +117,29 @@ class FragmentFirst : Fragment() {
 
             button2.setOnClickListener {
 
-                if (Permissions().hasPermissions(requireContext())){
-                    binding.tvText.text = "allowed"
-                    WriteOpenExternalMemory(requireContext()).writeToExternalMemory()
-                    }else{Permissions().requestPermissions(requireContext() as Activity, PERMISSION_STORAGE)}
+                val state = Environment.getExternalStorageState()
 
-//                binding.tvText.text = Permissions().hasPermissions(requireContext()).toString()
+//                val fullpath = "/sdcard/Android/data"
+
+                if (hasWritePermissions) {
+
+                    if (state == Environment.MEDIA_MOUNTED) {
+
+                        val file = File(
+                            requireContext().getExternalFilesDir(null),
+                            FILE_EXTERNAL_NAME
+                        )
+
+                        BufferedWriter(FileWriter(file)).use {
+                            it.write("cjtyjtdydydtydytd")
+                        }
 
 
-//                binding.tvText.text =
-//                    (requireContext()
-//                        .hasPermission(
-//                            Manifest.permission.WRITE_EXTERNAL_STORAGE
-//                        ))
-//                        .toString()
+                    } else {
+                        binding.tvText.text = "denied"
+                    }
+
+                }
             }
         }
     }
@@ -211,7 +235,6 @@ class FragmentFirst : Fragment() {
         }
     }
 
-
     companion object {
         const val FILE_NAME = "File_Storage"
 
@@ -224,10 +247,10 @@ class FragmentFirst : Fragment() {
 
 }
 
-//fun Context.hasPermission(permission: String): Boolean {
-//    return ContextCompat.checkSelfPermission(
-//        this,
-//        permission
-//    ) == PackageManager.PERMISSION_GRANTED
-//}
+fun Context.hasPermission(permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        this,
+        permission
+    ) == PackageManager.PERMISSION_GRANTED
+}
 
